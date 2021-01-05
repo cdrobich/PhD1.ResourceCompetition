02aafb57f44756dcd7315f571b9baa83827081e6
Courtney Robichaud <cdrobich@gmail.com>
2021-01-05 15:11:50 -0500

    re-did niche rover with differnt PCA results, scaled PCA scores to prop. variance explained by each axis

--- Scripts/nicheROVER.R
+++ Scripts/nicheROVER.R
@@ -1,10 +1,14 @@
 library(nicheROVER)
 
-data <- read.csv("Data/PCA scores_123.csv")
-data$Species <- as.factor(data$Species)
+data.raw <- read.csv("Data/PCA scores_123.csv") # raw PCA scores
+data.raw$Species <- as.factor(data.raw$Species)
 
-data <- data %>% relocate(Species, .before = PC1)
-data <- data[,2:5]
+data.raw <- data.raw %>% relocate(Species, .before = PC1)
+data.raw <- data.raw %>% select(Species:PC3)
+
+
+data <- read.csv("Data/PCA scores_123_adjusted.csv") # PCA scores x prop. variance explained
+data$Species <- as.factor(data$Species)
 
 str(data)
 
@@ -71,27 +75,26 @@ over.stat <- overlap(data.par, nreps = nsamples, nprob = 1000, alpha = c(0.95,
 # The mean overlap metrics calculated across iteratations for both niche
 # region sizes (alpha = .95 and alpha = .99) can be calculated and displayed
 # in an array.
-over.mean <- apply(over.stat, c(1:2, 4), mean) * 100
+over.mean <- apply(over.stat, c(1:2, 4), median) * 100
 round(over.mean, 2)
-
 
 #, , alpha = 95%
 
-                              #Species B
+#                              Species B
 #Species A       Calamagrostis Carex Phragmites Typha
-#Calamagrostis            NA 46.68       4.75  3.50
-#Carex                  4.75    NA      43.69 25.78
-#Phragmites             0.68 70.98         NA 56.24
-#Typha                  0.28 21.81      46.61    NA
+#Calamagrostis            NA  25.50        0.3    0.0
+#Carex                  1.85    NA       70.0    29.1
+#Phragmites             0.10  55.85         NA   41.0
+#Typha                  0.00  17.80       51.8    NA
 
 #, , alpha = 99%
 
-                              #Species B
+#                                   Species B
 #Species A       Calamagrostis Carex Phragmites Typha
-#Calamagrostis            NA  73.96      15.94 13.42
-#Carex                  7.57    NA      56.65 42.48
-#Phragmites             1.28  84.37         NA 72.47
-#Typha                  0.49  36.30      64.87    NA
+#Calamagrostis            NA  60.8       8.30    2.50
+#Carex                   3.6    NA      83.55   50.95
+#Phragmites              0.2  72.0         NA   57.75
+#Typha                   0.1  31.9      70.25    NA
 
 over.cred <- apply(over.stat * 100, c(1:2, 4), quantile, prob = c(0.025, 0.975), 
                    na.rm = TRUE)
@@ -99,35 +102,34 @@ round(over.cred[, , , 1])  # display alpha = .95 niche region
 
 #, , Species B = Calamagrostis
 
-# Species A
-#           Calamagrostis Carex Phragmites Typha
-# 2.5%             NA     0          0     0
-# 97.5%            NA    14          4     2
+#Species A
+#Calamagrostis Carex Phragmites Typha
+#2.5%             NA     0          0     0
+#97.5%            NA    10          2     1
 
 #, , Species B = Carex
 
-# Species A
-#           Calamagrostis Carex Phragmites Typha
-# 2.5%              2    NA         44     3
-# 97.5%            93    NA         95    56
+#Species A
+#Calamagrostis Carex Phragmites Typha
+#2.5%              0    NA         30     4
+#97.5%            87    NA         84    54
 
 #, , Species B = Phragmites
 
-# Species A
-#           Calamagrostis Carex Phragmites Typha
-# 2.5%              0    21         NA    19
-# 97.5%            36    71         NA    77
+#Species A
+#Calamagrostis Carex Phragmites Typha
+#2.5%              0    44         NA    25
+#97.5%            45    93         NA    84
 
 #, , Species B = Typha
 
-#    Species A
-#           Calamagrostis Carex Phragmites Typha
-# 2.5%              0     4         25    NA
-# 97.5%            32    61         88    NA
+#Species A
+#Calamagrostis Carex Phragmites Typha
+#2.5%              0     6         17    NA
+#97.5%            46    74         77    NA
 
 
-# Overlap plot.Before you run this, make sure that you have chosen your
-# alpha level.
+# Overlap plot with 95% alpha
 
 over.stat <- overlap(data.par, nreps = nsamples, nprob = 1000, alpha = 0.95)
 overlap.plot(over.stat, col = clrs, mean.cred.col = "black", equal.axis = TRUE, 
