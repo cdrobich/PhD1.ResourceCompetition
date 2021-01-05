c4833321b6945b097099e2bd4578c3733c64a480
Courtney Robichaud <cdrobich@gmail.com>
2021-01-05 15:11:07 -0500

    Added moisture to PCA, changed labels in data, made figure for PCA

--- Scripts/Soil nutrient.R
+++ Scripts/Soil nutrient.R
@@ -2,20 +2,22 @@ library(tidyverse)
 library(vegan)
 library(ggrepel)
 library(Hmisc)
+library(ggpubr)
 
 soil <- read.csv("Data/soil_nutrients.csv")
 dim(soil)
 colnames(soil)
 
-soils <- soil[,3:17] # just the soil nutrient data
-env <- soil[,1:2] # species is the group factor
+soils <- soil %>% select(Moisture:Light)# just the soil nutrient data
+env <- soil %>% select(Vegetation:Species) # species is the group factor
 
 hist.data.frame(soils)
 
 # CHECK THEM AND LOG + 10 TRANSFORM
 
 soils.log <- soils %>% 
-  mutate(Cu.log = log(Cu+1),
+  mutate(Moist.log = log(Moisture -1),
+         Cu.log = log(Cu+1),
          Fe.log = log(Fe+1),
          Mn.log = log(Mn+1),
          P.log = log(P+1),
@@ -31,7 +33,7 @@ soils.log <- soils %>%
          Light.log= log(Light+1))
 
 
-soils.log <- soils.log[,16:29]
+soils.log <- soils.log %>% select(Moist.log:Light.log)
 
 soils.log$pH <- soil$pH
 
@@ -42,21 +44,24 @@ summary(pca)
 
 summary(eigenvals(pca))
 
-
-
 #Importance of components:
-#                        PC1     PC2     PC3     PC4     PC5     PC6     PC7     PC8
-#Eigenvalue            10.6224 1.23140 1.01816 0.56270 0.39867 0.30950 0.26292 0.15434
-#Proportion Explained   0.7082 0.08209 0.06788 0.03751 0.02658 0.02063 0.01753 0.01029
-#Cumulative Proportion  0.7082 0.79025 0.85813 0.89564 0.92222 0.94285 0.96038 0.97067
 
-#                       PC9    PC10     PC11     PC12     PC13     PC14      PC15
-#Eigenvalue            0.148458 0.08805 0.080003 0.062670 0.028124 0.017946 0.0146728
-#Proportion Explained  0.009897 0.00587 0.005334 0.004178 0.001875 0.001196 0.0009782
-#Cumulative Proportion 0.980569 0.98644 0.991772 0.995950 0.997825 0.999022 1.0000000
+#                        PC1     PC2     PC3     PC4     PC5     PC6     PC7
+#Eigenvalue            10.6775 1.28773 1.05527 0.90061 0.55561 0.38778 0.30447
+#Proportion Explained   0.6673 0.08048 0.06595 0.05629 0.03473 0.02424 0.01903
+#Cumulative Proportion  0.6673 0.74783 0.81378 0.87007 0.90480 0.92903 0.94806
 
+#PC8      PC9     PC10     PC11   PC12     PC13     PC14
+#Eigenvalue            0.24715 0.148847 0.146857 0.087963 0.0800 0.062438 0.025712
+#Proportion Explained  0.01545 0.009303 0.009179 0.005498 0.0050 0.003902 0.001607
+#Cumulative Proportion 0.96351 0.972812 0.981990 0.987488 0.9925 0.996390 0.997997
 
+#PC15      PC16
+#Eigenvalue            0.017380 0.0146676
+#Proportion Explained  0.001086 0.0009167
+#Cumulative Proportion 0.999083 1.0000000
 
+par(1,1)
 screeplot(pca, bstick = TRUE, type = "l",
           main = NULL)
 
@@ -69,7 +74,7 @@ pca.scores.sites$Species <- as.factor(env$Species) # add group factor
 write.csv(pca.scores.sites,"Data/PCA scores_123.csv") #use this with NicheRover
 
 
-pca.scores.spp <- as.data.frame(scores(pca, display = c("species"))) # vectors
+pca.scores.spp <- as.data.frame(scores(pca, choices = c(1,2,3), display = c("species"))) # vectors
 pca.scores.spp$labels <- row.names(pca.scores.spp) # adding in as the label
 
 write.csv(pca.scores.spp, "Data/PCA scores_spp.csv")
@@ -103,8 +108,8 @@ p <-
                  aes(x = PC1, y = PC2, label = labels),
                  color="black",
                  size = 6) +
-  #ylim(-2, 2) +
-  xlim(-2, 2.2) +
+  #ylim(-2, 2.2) +
+  #xlim(-2, 2.2) +
   theme_classic() +
   scale_shape_manual(values = c(0, 1, 2, 5)) +
   scale_colour_manual(values = c("#525252","#9ecae1",
@@ -112,14 +117,56 @@ p <-
   geom_hline(yintercept=0, linetype="dotted") +
   geom_vline(xintercept=0, linetype="dotted") +
   coord_fixed() +
-  labs(x = "PC1 (0.713)",
-       y = "PC2 (0.087)") 
+  labs(x = "PC1 (0.667)",
+       y = "PC2 (0.080)") 
 
 p
 
 
-ggsave("Figures/PCA_soil.TIFF", p,
+
+
+
+p3 <-
+  pca.sites %>%
+  ggplot()+
+  geom_point(aes(x = PC1, y = PC3, 
+                 colour = Species,
+                 shape = Species), size = 5, stroke = 1.5) +
+  stat_ellipse(data = pca.sites,
+               aes(x = PC1, y = PC3, 
+                   colour = Species, 
+                   linetype = Species)) +
+  geom_segment(data = pca.spp, 
+               aes(x = 0, xend = PC1, 
+                   y = 0, yend = PC3), 
+               arrow = arrow(length = unit(0.35, "cm"), # adjust the arrow head size here
+                             type="closed"), # can change to open 
+               color = "black") + 
+  geom_text_repel(data = pca.spp, 
+  aes(x = PC1, y = PC3, label = labels),
+  color="black",
+  size = 6) +
+  #ylim(-2, 2) +
+  #xlim(-2, 2.2) +
+  theme_classic() +
+  scale_shape_manual(values = c(0, 1, 2, 5)) +
+  scale_colour_manual(values = c("#525252","#9ecae1",
+                                 "#fb6a4a","#6a51a3")) +
+  geom_hline(yintercept=0, linetype="dotted") +
+  geom_vline(xintercept=0, linetype="dotted") +
+  coord_fixed() +
+  labs(x = "PC1 (0.667)",
+       y = "PC3 (0.066)") 
+
+p3
+
+
+PCA <- ggarrange(p,p3,
+          common.legend = TRUE,
+          labels = "AUTO",
+          legend = "bottom",
+          align = c("hv"))
+
+
+ggsave("Figures/PCA_soil_axes.TIFF", PCA,
        dpi = 300)
-
-
-
